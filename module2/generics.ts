19f30668a6a78a0f3ad1d6827973ec9795a97dbd
Arnab Gupta <arnabgupta1111@gmail.com>
2024-11-05 11:55:09 +0600

    Learn keyof keyword and contrains in generics type

--- module2/generics.ts
+++ module2/generics.ts
@@ -14,6 +14,94 @@
     },
   ];
   //Tuple
-  type GenericTuple<U,V>=[U,V];
-  const userWithId=["#12345",{name:"Kabul",age:34}]
+  type GenericTuple<U, V> = [U, V];
+  const userWithId = ["#12345", { name: "Kabul", age: 34 }];
+
+  //Interface
+  interface Developer<T, X = null> {
+    name: string;
+    age: number;
+    device: {
+      brand: string;
+      model: string;
+    };
+    smartWatch: T;
+    bike?: X;
+  }
+  interface poorWatch {
+    brand: string;
+    model: string;
+  }
+  interface poshWatch {
+    brand: string;
+    model: string;
+    display: string;
+  }
+  interface royalBike {
+    brand: string;
+    storage: string;
+  }
+  let poorDeveloper: Developer<poorWatch> = {
+    name: "Patel",
+    age: 25,
+    device: {
+      brand: "ASUS",
+      model: "Gajati",
+    },
+    smartWatch: {
+      brand: "devtab",
+      model: "k2j2",
+    },
+  };
+
+  let richDeveloper: Developer<poshWatch, royalBike> = {
+    name: "Abul",
+    age: 25,
+    device: {
+      brand: "HP",
+      model: "boss",
+    },
+    smartWatch: {
+      brand: "apple",
+      model: "x45g6",
+      display: "SuperAmoled",
+    },
+    bike: {
+      brand: "Royal Enfield",
+      storage: "300cc",
+    },
+  };
+  console.log(poorDeveloper);
+  console.log(richDeveloper);
+
+  //Function
+  interface CourseId {
+    id: string;
+  }
+  const addCourseToStudent = <T extends CourseId>(student: T): T & { course: string } => {
+    const course = "Web Development";
+    return {
+      ...student,
+      course,
+    };
+  };
+  interface Student1 {
+    id: string;
+    name: string;
+    email: string;
+    age: number;
+  }
+  const student1 = addCourseToStudent<Student1>({
+    id: "#4546",
+    name: "Babul",
+    email: "babul@gmail.com",
+    age: 23,
+  });
+
+  //Constrains example
+  function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+  let person = { name: "Rajat", age: 25 };
+  getProperty(person, "name");
 }
